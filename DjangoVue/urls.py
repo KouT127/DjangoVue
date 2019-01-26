7396e261d2f7a76a58620c8f3e39f3e65c054de7
KouT127 <kou26haru@gmail.com>
2019-01-26 19:50:48 +0900

    add user api

--- DjangoVue/urls.py
+++ DjangoVue/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url,include
+from users.urls import router as user_router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^api/', include(user_router.urls)),
 ]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=32)
+    email = models.EmailField(max_length=254)
+    description = models.TextField()
+    website = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
