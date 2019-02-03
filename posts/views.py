f2404e658f680ed8f89e8a5e0bd5a013f1868cb0
KouT127 <kou26haru@gmail.com>
2019-02-03 21:31:07 +0900

    add posts create method

--- posts/views.py
+++ posts/views.py
@@ -3,23 +3,22 @@ from rest_framework import status
 from rest_framework import viewsets, filters
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from .models import Post
 from .serializer import PostSerializer, AddPostSerializer
+import logging
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    # Get
+    logger = logging.getLogger(__name__)
+    logger.error('Posts get')
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    @detail_route(methods=['post'])
-    def add_post(self, request, pk):
-        user = request.user 
-        # user = self.get_object()
-        serializer = AddPostSerializer(data=request.data)
-        if serializer.is_valid():
-            user.post_set.create(content=serializer.Meta)
-            user.save()
-            return Response({'status': 'password set'})
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+    # Post
+    def create(self, request):
+        logger = logging.getLogger(__name__)
+        logger.error('Posts post')
+        user = request.user
+        return Response({'user': user.username})
