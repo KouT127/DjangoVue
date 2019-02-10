c4203f9d13ca898778a1d3af64d35e081df32ec5
KouT127 <kou26haru@gmail.com>
2019-02-10 23:41:54 +0900

    mod 不要部を削除、コメントを追加

--- users/serializer.py
+++ users/serializer.py
@@ -4,7 +4,6 @@ from .models import User
 
 # 必要なデータをfieldsに定義
 class UserSerializer(serializers.ModelSerializer):
-    # image = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
     image_url = serializers.SerializerMethodField()
     class Meta:
         model = User
@@ -17,12 +16,10 @@ class UserSerializer(serializers.ModelSerializer):
             'image_url'
         )
         read_only_fields = ('image_url',)
+        # fieldsには、記述するが返したくない場合下記に記入
         extra_kwargs = {
             'image': {'write_only': True}
         }
 
     def get_image_url(self, obj):
         return obj.image.url
-        # request = self.context.get('request')
-        # image_url = user.image.url
-        # return request.build_absolute_uri(image_url)
